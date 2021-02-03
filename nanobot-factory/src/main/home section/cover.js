fb0e1312940b5183cf470f276b5756b3b033450c
Robby-Codes <contact@robertoguerra.info>
2021-02-02 21:11:06 -0500

    Animations for cover page

--- nanobot-factory/src/main/home section/cover.js
+++ nanobot-factory/src/main/home section/cover.js
@@ -20,10 +20,163 @@ const CoverPage = () => {
           </div>
           <div className="dec-line"></div>
         </div>
-        <p>
-          Create as many nanobots as possible! Make sure to purchase upgrades
-          along the way to speed up your progress. You can check out your stats
-          at any point as well.
+        <p className="description">
+          <span>C</span>
+          <span>r</span>
+          <span>e</span>
+          <span>a</span>
+          <span>t</span>
+          <span>e</span>
+          <span className="space"> </span>
+          <span>a</span>
+          <span>s</span>
+          <span className="space"> </span>
+          <span>m</span>
+          <span>a</span>
+          <span>n</span>
+          <span>y</span>
+          <span className="space"> </span>
+          <span>n</span>
+          <span>a</span>
+          <span>n</span>
+          <span>o</span>
+          <span>b</span>
+          <span>o</span>
+          <span>t</span>
+          <span>s</span>
+          <span className="space"> </span>
+          <span>a</span>
+          <span>s</span>
+          <span className="space"> </span>
+          <span>p</span>
+          <span>o</span>
+          <span>s</span>
+          <span>s</span>
+          <span>i</span>
+          <span>b</span>
+          <span>l</span>
+          <span>e</span>
+          <span>!</span>
+          <br></br>
+          <span>M</span>
+          <span>a</span>
+          <span>k</span>
+          <span>e</span>
+          <span className="space"> </span>
+          <span>s</span>
+          <span>u</span>
+          <span>r</span>
+          <span>e</span>
+          <span className="space"> </span>
+          <span>t</span>
+          <span>o</span>
+          <span className="space"> </span>
+          <span>p</span>
+          <span>u</span>
+          <span>r</span>
+          <span>c</span>
+          <span>h</span>
+          <span>a</span>
+          <span>s</span>
+          <span>e</span>
+          <span className="space"> </span>
+          <span>u</span>
+          <span>p</span>
+          <span>g</span>
+          <span>r</span>
+          <span>a</span>
+          <span>d</span>
+          <span>e</span>
+          <span>s</span>
+          <span className="space"> </span>
+          <span>a</span>
+          <span>l</span>
+          <span>o</span>
+          <span>n</span>
+          <span>g</span>
+          <span className="space"> </span>
+          <span>t</span>
+          <span>h</span>
+          <span>e</span>
+          <span className="space"> </span>
+          <span>w</span>
+          <span>a</span>
+          <span>y</span>
+          <br></br>
+          <span>t</span>
+          <span>o</span>
+          <span className="space"> </span>
+          <span>s</span>
+          <span>p</span>
+          <span>e</span>
+          <span>e</span>
+          <span>d</span>
+          <span className="space"> </span>
+          <span>u</span>
+          <span>p</span>
+          <span className="space"> </span>
+          <span>y</span>
+          <span>o</span>
+          <span>u</span>
+          <span>r</span>
+          <span className="space"> </span>
+          <span>p</span>
+          <span>r</span>
+          <span>o</span>
+          <span>g</span>
+          <span>r</span>
+          <span>e</span>
+          <span>s</span>
+          <span>s</span>
+          <span>.</span>
+          <span className="space"> </span>
+          <span>Y</span>
+          <span>o</span>
+          <span>u</span>
+          <span className="space"> </span>
+          <span>c</span>
+          <span>a</span>
+          <span>n</span>
+          <span className="space"> </span>
+          <span>a</span>
+          <span>l</span>
+          <span>s</span>
+          <span>o</span>
+          <span className="space"> </span>
+          <span>c</span>
+          <span>h</span>
+          <span>e</span>
+          <span>c</span>
+          <span>k</span>
+          <span className="space"> </span>
+          <span>o</span>
+          <span>u</span>
+          <span>t</span>
+          <br></br>
+          <span>y</span>
+          <span>o</span>
+          <span>u</span>
+          <span>r</span>
+          <span className="space"> </span>
+          <span>s</span>
+          <span>t</span>
+          <span>a</span>
+          <span>t</span>
+          <span>s</span>
+          <span className="space"> </span>
+          <span>a</span>
+          <span>t</span>
+          <span className="space"> </span>
+          <span>a</span>
+          <span>n</span>
+          <span>y</span>
+          <span className="space"> </span>
+          <span>p</span>
+          <span>o</span>
+          <span>i</span>
+          <span>n</span>
+          <span>t</span>
+          <span>.</span>
         </p>
         <button onClick={() => handleClick()}>LETS GO!</button>
       </div>
