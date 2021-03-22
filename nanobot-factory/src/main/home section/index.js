9735f4b2ef1235c92b5ed047de44ad2784e637ca
Robby-Codes <contact@robertoguerra.info>
2021-03-22 16:29:08 -0400

    Better rising number code and animation

--- nanobot-factory/src/main/home section/index.js
+++ nanobot-factory/src/main/home section/index.js
@@ -35,34 +35,15 @@ window.addEventListener("resize", () => checkWindowHeight());
 // Displays click counter with amount and "clicker" button
 const Clicker = () => {
   const [Amount, setAmount] = useState(0);
-  const [number_1, setNumber_1] = useState();
-  const [number_2, setNumber_2] = useState();
-  const [number_3, setNumber_3] = useState();
-  const [number_4, setNumber_4] = useState();
-  const [number_5, setNumber_5] = useState();
-  const [number_6, setNumber_6] = useState();
-  const [number_7, setNumber_7] = useState();
-  const [number_8, setNumber_8] = useState();
-  const [number_9, setNumber_9] = useState();
-  const [number_10, setNumber_10] = useState();
+  const [numbers, setNumbers] = useState([null]);
   // Rerenders numbers to be animated on click
   const handleClick = () => {
     data.total_clicks += 1;
     data.current_amount = Amount + data.manual_value;
-    numberOrder(
-      setNumber_1,
-      setNumber_2,
-      setNumber_3,
-      setNumber_4,
-      setNumber_5,
-      setNumber_6,
-      setNumber_7,
-      setNumber_8,
-      setNumber_9,
-      setNumber_10
-    );
+    numberOrder(numbers, setNumbers);
     setAmount(data.current_amount);
   };
+  console.log(numbers);
   return (
     <>
       <div
@@ -78,16 +59,9 @@ const Clicker = () => {
         >
           <img className="click-icon" src={images.click_icon} alt="A nanobot" />
         </button>
-        {number_1}
-        {number_2}
-        {number_3}
-        {number_4}
-        {number_5}
-        {number_6}
-        {number_7}
-        {number_8}
-        {number_9}
-        {number_10}
+        {numbers.map((number) => {
+          return number;
+        })}
       </div>
       <UpgradeSection updateAmount={() => setAmount(data.current_amount)} />
     </>
@@ -121,18 +95,7 @@ const RisingNumbers = ({ xaxis, number, key }) => {
 
 // Called on click to render a RisingNumbers component
 let rising_count = 0;
-const numberOrder = (
-  setNumber_1,
-  setNumber_2,
-  setNumber_3,
-  setNumber_4,
-  setNumber_5,
-  setNumber_6,
-  setNumber_7,
-  setNumber_8,
-  setNumber_9,
-  setNumber_10
-) => {
+const numberOrder = (numbers, setNumbers) => {
   rising_count += 1;
   let new_xaxis = 0;
   // Decides whether to render RisingNumbers component on the left
@@ -142,98 +105,20 @@ const numberOrder = (
   } else if (rising_count % 2 !== 0) {
     new_xaxis = Math.floor(Math.random() * 23) + 5;
   }
-  if (rising_count === 1) {
-    setNumber_1(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={1}
-      />
-    );
-    setNumber_2();
-  } else if (rising_count === 2) {
-    setNumber_2(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={2}
-      />
-    );
-    setNumber_3();
-  } else if (rising_count === 3) {
-    setNumber_3(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={3}
-      />
-    );
-    setNumber_4();
-  } else if (rising_count === 4) {
-    setNumber_4(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={4}
-      />
-    );
-    setNumber_5();
-  } else if (rising_count === 5) {
-    setNumber_5(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={5}
-      />
-    );
-    setNumber_6();
-  } else if (rising_count === 6) {
-    setNumber_6(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={6}
-      />
-    );
-    setNumber_7();
-  } else if (rising_count === 7) {
-    setNumber_7(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={7}
-      />
-    );
-    setNumber_8();
-  } else if (rising_count === 8) {
-    setNumber_8(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={8}
-      />
-    );
-    setNumber_9();
-  } else if (rising_count === 9) {
-    setNumber_9(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={9}
-      />
-    );
-    setNumber_10();
-  } else if (rising_count === 10) {
-    setNumber_10(
-      <RisingNumbers
-        xaxis={new_xaxis.toString() + "%"}
-        number={formatNumbers(data.manual_value)}
-        key={10}
-      />
-    );
-    setNumber_1();
-    rising_count = 0;
+  // Using setNumbers to add new component to numbers array causing a rerender.
+  // Also stopping the array from getting too long.
+  let arr = numbers;
+  if (arr.length > 20) {
+    arr.shift();
   }
+  arr.push(
+    <RisingNumbers
+      xaxis={new_xaxis.toString() + "%"}
+      number={formatNumbers(data.manual_value)}
+      key={rising_count}
+    />
+  );
+  setNumbers(arr);
 };
 
 export default HomePage;
